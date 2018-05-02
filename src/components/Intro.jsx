45b07f3743d438f7f2ebdcc42e14b5c4201de2bd
camille-s <camille.seaberry@gmail.com>
2018-05-02 14:09:39 -0400

    Finish draft 2

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,14 +8,6 @@ import md from '../data/intro.js';
 const Intro = ({ isOpen, handleClick }) => (
 	<div className="Intro">
 		<Header size="huge" as="h1">Housing segregation in Greater New Haven, yesterday and today</Header>
-		{/* <Accordion>
-			<Accordion.Title active={isOpen} onClick={handleClick} as={Button} size="tiny">
-				About this story
-			</Accordion.Title>
-			<Accordion.Content active={isOpen}>
-				<ReactMarkdown source={md} />
-			</Accordion.Content>
-		</Accordion> */}
 		<Accordion>
 			<Accordion.Title active={isOpen} index={0} onClick={handleClick} as={Button} size="tiny">
 				<Icon name="dropdown" />
